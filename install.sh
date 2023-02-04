fb8e3be922fae2d9e94103cd6a4ef124821fadb5
dp964 <dp964@users.noreply.github.com>
2023-02-04 18:50:56 +1100

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+SERVERNAME=$(cat /etc/hostname) # in case we haven't rebooted the box after updating the hostname
+
 ### Installing Packages
 echo ">>> INSTALLING REQUIRED PACKAGES"
 
 # Install Dev Tools
 yum groupinstall -y 'Development Tools'
 yum install -y nano
-dnf makecache --refreshh
+dnf makecache --refresh
 dnf config-manager --set-enabled crb
 
 # Install Mongo DB
@@ -34,11 +36,23 @@ yum install -y httpd
 systemctl enable httpd
 systemctl start httpd
 
+# Install OpenSSL FIPS
+
+cd ~/
+wget https://www.openssl.org/source/openssl-1.1.1s.tar.gz
+tar -xzvf openssl-1.0.2e.tar.gz
+cd ~/openssl-1.0.2e
+./config fips 
+make depend
+make 	
+make install
+
 # Install Development Packages
+dnf install -y wget
 dnf install -y libevent
 dnf install -y libevent-devel
 dnf install -y meson
-dnf install -y wget
+
 dnf install -y python3-pip
 dnf install -y augeas-libs
 dnf install -y mod_ssl
@@ -46,6 +60,8 @@ dnf install -y cmake
 dnf install -y libmicrohttpd-devel
 dnf install -y jansson-devel
 dnf install -y openssl-devel
+
+
 dnf install -y libsrtp-devel
 dnf install -y glib2-devel
 dnf install -y opus-devel
@@ -70,13 +86,17 @@ cd ~/usrsctp
 ./bootstrap
 ./configure
 make
+make install
 cd ~/
+
 # Install Sofia
 
 git clone https://github.com/freeswitch/sofia-sip.git
 cd ~/sofia-sip
+sh autogen.sh
 ./configure
 make
+make install
 cd ~/
 
 # Other packages
@@ -99,68 +119,74 @@ ln -s /snap/bin/certbot /usr/bin/certbot
 systemctl list-timers
 
 #### Configuring Virtual Hosts
-echo $HOSTNAME
-cat << @EOF > /etc/httpd/conf/${HOSTNAME}.conf
+echo $SERVERNAME
+cat << @EOF > /etc/httpd/conf/${SERVERNAME}.conf
 <VirtualHost *:80>
-    ServerName ${HOSTNAME}
+    ServerName ${SERVERNAME}
     DocumentRoot /var/www/html
-    ServerAlias ${HOSTNAME}
+    ServerAlias ${SERVERNAME}
 RewriteEngine on
-RewriteCond %{SERVER_NAME} =${HOSTNAME}
+RewriteCond %{SERVER_NAME} =${SERVERNAME}
 RewriteRule ^ https://%{SERVER_NAME}%{REQUEST_URI} [END,NE,R=permanent]
 </VirtualHost>
 
 <IfModule mod_ssl.c>
 <VirtualHost *:443>
-        ServerName ${HOSTNAME}
+        ServerName ${SERVERNAME}
         DocumentRoot /var/www/html
-        ServerAlias ${HOSTNAME}
+        ServerAlias ${SERVERNAME}
 
-        SSLCertificateFile /etc/letsencrypt/live/${HOSTNAME}/fullchain.pem
-        SSLCertificateKeyFile /etc/letsencrypt/live/${HOSTNAME}/privkey.pem
+        SSLCertificateFile /etc/letsencrypt/live/${SERVERNAME}/fullchain.pem
+        SSLCertificateKeyFile /etc/letsencrypt/live/${SERVERNAME}/privkey.pem
         Include /etc/letsencrypt/options-ssl-apache.conf
 </VirtualHost>
 </IfModule>
 @EOF
 
-cat << @EOF > /etc/httpd/conf/api.${HOSTNAME}.conf
+cat << @EOF > /etc/httpd/conf/api.${SERVERNAME}.conf
 Listen 80
 Listen 443
 <VirtualHost *:80>
-        ServerName api.${HOSTNAME}
+        ServerName api.${SERVERNAME}
         DocumentRoot /var/www/html/api
-        ServerAlias api.${HOSTNAME}
+        ServerAlias api.${SERVERNAME}
         RewriteEngine on
-        RewriteCond %{SERVER_NAME} =api.${HOSTNAME}
+        RewriteCond %{SERVER_NAME} =api.${SERVERNAME}
         RewriteRule ^ https://%{SERVER_NAME}%{REQUEST_URI} [END,NE,R=permanent]
 </VirtualHost>
 
 <IfModule mod_ssl.c>
 <VirtualHost *:443>
-        ServerName api.${HOSTNAME}
+        ServerName api.${SERVERNAME}
         DocumentRoot /var/www/html/api
-        ServerAlias api.${HOSTNAME}
+        ServerAlias api.${SERVERNAME}
 
-        SSLCertificateFile /etc/letsencrypt/live/api.${HOSTNAME}/fullchain.pem
-        SSLCertificateKeyFile /etc/letsencrypt/live/api.${HOSTNAME}/privkey.pem
+        SSLCertificateFile /etc/letsencrypt/live/api.${SERVERNAME}/fullchain.pem
+        SSLCertificateKeyFile /etc/letsencrypt/live/api.${SERVERNAME}/privkey.pem
         Include /etc/letsencrypt/options-ssl-apache.conf
 </VirtualHost>
 </IfModule>
 @EOF
 
 ### Configuring Certbot
-certbot certonly -d ${HOSTNAME} -d www.${HOSTNAME}
-certbot certonly -d api.${HOSTNAME} -d www.api.${HOSTNAME}
+certbot certonly -d ${SERVERNAME} -d www.${SERVERNAME}
+certbot certonly -d api.${SERVERNAME} -d www.api.${SERVERNAME}
 
-systemctl reload httpd
+systemctl restart httpd
+
+## Rollback OpenSSL to known working version and install compat layer
+yum downgrade openssl-3.0.1-41.el9
+dnf install compat-openssl11
+
+systemctl restart httpd
 
 ### Configuring Coturn
-cd ~/
-git clone https://github.com/coturn/coturn.git
+
+wget https://coturn.net/turnserver/v4.5.2/turnserver-4.5.2.tar.gz
+tar -xf turnserver-4.5.2.tar.gz
+mv turnserver-4.5.2 coturn
 cd ~/coturn
 ./configure
 make
+make install
 cd ~/
-
-systemctl enable coturn.service
-systemctl reload httpd.service
